e02e3cad20975f3a422b455161cbe5850f985999
Alexander Grube <alexp.grube@googlemail.com>
2021-01-20 19:18:53 +0100

    fixed another typo

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func setupRoutes(app *fiber.App) {
 
 func initDatabase() {
 	var err error
-	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	database.DBConn, err = gorm.Open("sqlite3", "gophers.db")
 	if err != nil {
 		panic("failed to connect database")
 	}
